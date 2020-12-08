066bfd98eff7c6abda6f4f1825157fd62d00df91
akshayn963 <akshaynemade9597@gmail.com>
2020-12-08 21:38:27 +0530

    updated Assingment 7...

--- Day8/Lambda.cs
+++ Day8/Lambda.cs
@@ -13,31 +13,33 @@ namespace Assignment7
             Employee e = new Employee();
             
             Func<decimal, decimal, decimal, decimal> s1 = (P, N, R) => (P * R * N) / 100;
-            Console.WriteLine(s1(100,5,10));
+            Console.WriteLine("Simple Interest = "+s1(100,5,10));
 
             Console.WriteLine();
             Console.WriteLine();
 
             Func<int, int, bool> b1 = (a, b) => (a > b);
-            Console.WriteLine(b1(10,20));
+            Console.WriteLine("IsGreater = "+b1(10,20));
 
             Console.WriteLine();
             Console.WriteLine();
 
             Func<Employee, decimal> bs = e1 => e1.basic;
-            Console.WriteLine(bs(e));
+            Console.WriteLine("Basic Salary = "+bs(e));
 
             Console.WriteLine();
             Console.WriteLine();
 
-            Func<int, bool> even = num => num % 2 == 0;
-            Console.WriteLine(even(13));
+            Predicate<int> even= num => num % 2 == 0;
+            //Func<int, bool> even = num => num % 2 == 0;
+            Console.WriteLine("IsEven = "+even(13));
 
             Console.WriteLine();
             Console.WriteLine();
 
-            Func<Employee, bool> greater = e2 => e2.basic > 10000;
-            Console.WriteLine(greater(e));
+            Predicate<Employee> greater = e2 => e2.basic > 10000;
+           // Func<Employee, bool> greater = e2 => e2.basic > 10000;
+            Console.WriteLine("IsGreater = "+greater(e));
 
             Console.ReadLine();
         }
